4e30e335f82350c1da6e36aba6a73931662bb86a
vorgovo <32040628+vorgovo@users.noreply.github.com>
2017-12-10 19:50:01 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-  int foo() {
-    return 10;
+  public static int my_or (int[] arg) {
+    ans = 0;
+    for (int i = 0; i < arg.length; i++) {
+      ans = (ans | arg[i]);
+    }
+    return ans;
   }
 }
