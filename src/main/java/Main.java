f1ee5398b1886612b2264e3cb423fde098c5f470
vorgovo <32040628+vorgovo@users.noreply.github.com>
2017-12-18 09:43:00 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
   public static int rangeOr(int[] arg) {
-    int ans;
-    ans = 0;
+    int ans = 0;
     for (int i = 0; i < arg.length; i++) {
        ans = (ans | arg[i]);
     }
