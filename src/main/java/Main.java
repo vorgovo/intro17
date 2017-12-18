eac3bfbc15dff1c77177370d969c9f565c75ce51
vorgovo <32040628+vorgovo@users.noreply.github.com>
2017-12-18 09:55:04 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,21 @@
 public class Main {
-  public static int rangeOr(int[] arg) {
-    int ans;
-    ans = 0;
-    for (int i = 0; i < arg.length; i++) {
-       ans = (ans | arg[i]);
+    public static void main(String[] args) {
+        int[] input = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            try {
+                input[i] = Integer.parseInt(args[i]);
+            } catch (NumberFormatException error) {
+                System.out.println(String.format("Infernal error: wrong input: %s.", error.getMessage()));
+                return;
+            }
+        }
+        System.out.println(rangeOr(input));
     }
-    return ans;
-  }
+    public static int rangeOr(int[] arg) {
+        int ans = 0;
+        for (int i = 0; i < arg.length; i++) {
+            ans = (ans | arg[i]);
+        }
+        return ans;
+    }
 }
